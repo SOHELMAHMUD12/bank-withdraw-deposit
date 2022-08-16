586af4e4c82cbee38ea44cdaab0634c83a70b88b
SOHELMAHMUD12 <sohelcse123@gmail.com>
2022-08-17 00:28:04 +0600

    bank website

---
+++ js/withdraw.js
@@ -0,0 +1,48 @@
+// 1 add event handler with the withdraw button
+// 2 get the window amount from the withdraw input field
+// 2.5 also amke sure to convert the input into a number by using parseFloat
+// 3 get previous withdraw total
+// 4 calculate total withdraw amount
+// 4.5set total amount withdraw
+// 5 get previous balance total
+// 6 calculate new balance total
+// 6.5 set the new balance
+// 7 clear the input field
+// 1 step 1: 
+document.getElementById('btn-withdraw') .addEventListener('click', function(){
+    // console.log('withdraw button clicked');
+    // step 2
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithDrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithDrawAmountString);
+    // console.log(newWithdrawAmount);
+
+    // step 3
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+    // console.log(previousWithdrawTotal);
+
+    // step 4
+
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    // step 5
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    // console.log(previousBalanceTotal);
+
+    // step 6
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+ 
+
+
+    // step 7
+
+    withdrawField.value = '';
+
+})
+
